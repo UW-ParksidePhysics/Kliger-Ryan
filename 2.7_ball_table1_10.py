8ad255e3dcbc19e7ec6911a6520bc75c95326a9e
RyanKliger <klige001@rangers.uwp.edu>
2020-03-31 17:17:53 -0500

    Add ball table 2.7 to Github

--- 2.7_ball_table1_10.py
+++ 2.7_ball_table1_10.py
@@ -1,10 +1,24 @@
-time = []
-y = []
-v = 20
+#2.7 Ball Table
+def projectile(t,v0): # Function for projectile motion
+    return v0*t-0.5*g*t**2 #This is the function for projectile motion, when variables plugged in output is the result of this
+
+def change_in_time(n,v0): #Defining the parts of the list
+    tlist=[]
+    ylist=[]
+    t=0
+    dt=(2*v0/(g*n))
+    while t <=2*v0/g: #While t is less than the given value
+        y = projectile(t, v0)
+        tlist.append(t)  # tlist.append(2*v0/g)
+        ylist.append(y) # ylist.append(projectile_motion(2*v0/g,v0))
+        t=t+dt
+    return [tlist, ylist] #the outcome of following code is list of tlist and ylist
+
+#We need vale for v0, we will assume that the v0 is 500m/s
+v0=500
 g = 9.8
-tmax = 2 * v / g
-tmax = int(tmax)
-for t in range(0, tmax):
-    time.append(t)
-    y.append(v * t - (1 / 2) * g * t ** 2)
-print(time, y)
+n = 11 #We make n 11 because we want 10 elements
+
+listty=change_in_time(n,v0)
+print(listty[0]) #Listty left side
+print(listty[1]) #Listty right side

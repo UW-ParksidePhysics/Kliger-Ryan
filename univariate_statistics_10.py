dce3f14aa80206e3eed31d3a523ba4cb159f00a0
RyanKliger <klige001@rangers.uwp.edu>
2020-05-14 15:47:56 -0500

    Add to Github

--- univariate_statistics_10.py
+++ univariate_statistics_10.py
@@ -1,35 +1,37 @@
-import numpy as np
-data = np.array([[1.0,2.0],[3.0,4.0]])
-while True:
+import sys
+
+
+def univariate_statistics(data):
+    from numpy import std
+    un_data = zip(*data)
+    data = list(un_data)
     try:
-        u = data[0]
-        v = data[1]
-        break
+        min_x_value = min(data[0])  # Minimum x-value, built in maximum and minimum values
+        max_x_value = max(data[0])  # Maximum x-value
     except IndexError:
-        print('Data Array has inappropriate dimensions, please use array with two dimensions')
+        if not len(data) == 2:   # Error would except if there is more than two columns of data or less than 2
+            print("You only need two columns of data!")
+    try:
+        min_y_value = min(data[1])  # Minimum y-value, again built in minimum and maximum functions
+        max_y_value = max(data[1])  # Maximum y-value
+    except IndexError:  # Excepts error if length of data is less than one, no array
+        if len(data[0]) or len(data[1]) <=1:
+            print("You need more data points")
 
-#Mean
-mean2 = sum(v)/len(v)
+    # Mean
+    summation = sum(data[0]) + sum(data[1])
+    total = len(data[0]) + len(data[1])
+    mean = summation / total
+
+    # Standard deviation
+    standard_deviation = std(data)  # Somewhat built in function
+
+    # Statistics
+    statistics = [mean, standard_deviation, min_x_value, max_x_value, min_y_value, max_y_value]
+    return statistics
 
 
-print('The mean of the second sample is: ', mean2)
-#Standard Deviation
-variance2 = sum([((float(i) - mean2)**2) for i in v])/len(v)
-res2 = variance2**0.5
-print("Standard deviation of the second sample is: " + str(res2))
-
-#Max and Min
-maxu= max(u)
-maxv = max(v)
-print(maxu)
-print(maxv)
-
-minu = min(u)
-minv = min(v)
-print(minu)
-print(minv)
-
-values = np.array([mean2, res2, maxu, minu, maxv, minv])
-print(values)
+if __name__ == '__main__':  # For the module's name
+    print(univariate_statistics(sys.argv[1]))
 
 
